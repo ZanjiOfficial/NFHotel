12bc482f96a42208907c3204cab4821041bad8ba
Jakob Kelvin Jensen <jensenjakob64@gmail.com>
2026-09-02 16:27:15 +0200

    minor fixes

--- src/mobil_application/lib/utillity/string_extensions.dart
+++ src/mobil_application/lib/utillity/string_extensions.dart
@@ -1,15 +1,13 @@
 //code "stolen" from https://stackoverflow.com/questions/16800540/how-should-i-check-if-the-input-is-an-email-address-in-flutter
 
-import 'package:flutter/material.dart';
-
 extension StringExtensions on String {
   bool isValidEmail() {
     return RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$').hasMatch(this);
   }
 
   bool isWhiteSpace() {
-    return this.trim().isEmpty;
+    return trim().isEmpty;
   }
 
-  bool isValidPassword() => this.length >= 8;
+  bool isValidPassword() => length >= 8;
 }
