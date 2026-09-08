375ce1e33fa51a715dd639e8f3505e9341e259b0
Jakob Kelvin Jensen <jensenjakob64@gmail.com>
2026-09-08 15:25:29 +0200

    ryder op

--- src/mobil_application/lib/Presentation/LoginScreen.dart
+++ src/mobil_application/lib/Presentation/LoginScreen.dart
@@ -1,8 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:flutter_login/theme.dart';
+
+import 'RoomOverview.dart';
+
+//Fandt flutter_login https://pub.dev/packages/flutter_login#-installing-tab-
+//
+
+final users = {'user@example.com': '12345', 'user2@example.com': '54321'};
 
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  Duration get loginTime => const Duration(milliseconds: 300);
+
+  Future<String?> _authUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'User not found';
+      }
+      if (users[data.name] != data.password) {
+        return 'Invalid password';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(SignupData data) {
+    debugPrint('Signup data: $data');
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _recoverPassword(String name) {
+    debugPrint('name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'User not found';
+      }
+      return null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return FlutterLogin(
+      title: 'NFHotel',
+      logo: AssetImage('assets/logo.png'),
+      onLogin: _authUser,
+      onSignup: _signupUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => RoomOverview()),
+        );
+      },
+      onRecoverPassword: _recoverPassword,
+    );
   }
 }
