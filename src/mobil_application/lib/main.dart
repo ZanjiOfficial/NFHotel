04d4d27e3f226f0ce1c53ea1cf50222655d72bae
Jakob Kelvin Jensen <jensenjakob64@gmail.com>
2026-09-08 15:46:23 +0200

    login screen and navigator setup

--- src/mobil_application/lib/main.dart
+++ src/mobil_application/lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'Presentation/RoomOverview.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'Presentation/LoginScreen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: RoomOverview(),
-    );
+    return MaterialApp(home: LoginScreen());
   }
 }
